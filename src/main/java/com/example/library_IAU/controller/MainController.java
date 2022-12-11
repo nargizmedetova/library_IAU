2a1113edb4a38b9e6da081d293ddfa054081c87a
raiym2048 <raiymbek.daniiaruulu@iaau.edu.kg>
2022-12-11 14:22:25 +0600

    added sign in/up main pages

--- src/main/java/com/example/library_IAU/controller/MainController.java
+++ src/main/java/com/example/library_IAU/controller/MainController.java
@@ -1,15 +1,55 @@
 package com.example.library_IAU.controller;
 
 
+import com.example.library_IAU.model.UsersModel;
+import com.example.library_IAU.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
+    private final UsersService usersService;
+
+    public MainController(UsersService usersService) {
+        this.usersService = usersService;
+    }
 
     @GetMapping("/signIn")
     public String signIn(Model model){
+
+        model.addAttribute("signInRequest", new UsersModel());
         return "signIn";
+    }
+    @GetMapping("/signUp")
+    public String signUp(Model model){
+        model.addAttribute("signUpRequest", new UsersModel());
+
+        return "signUp";
+    }
+
+    @PostMapping("signUp")
+    public String postSignUp(@ModelAttribute UsersModel usersModel){
+        System.out.println("usersModel is:" + usersModel);
+        UsersModel registeredUsers = usersService.registerUser(usersModel.getEmail(), usersModel.getPassword());
+        return registeredUsers == null?"signUp":"signIn";
+    }
+    @PostMapping("signIn")
+    public String postSignIn(@ModelAttribute UsersModel usersModel){
+        System.out.println("usersModel is:" + usersModel);
+        UsersModel authenticate = usersService.authenticate(usersModel.getEmail(), usersModel.getPassword());
+        return authenticate == null?"signIn":"main";
+    }
+    @GetMapping("/main")
+    public String main(Model model){
+        return "main";
+    }
+
+    @GetMapping("/forgotPass")
+    public String forgotPass(Model model){
+        return "forgotPass";
     }
 }
