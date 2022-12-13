167bcf3fb3887295275db85fef7d3ce1ebb53ba3
raiym2048 <104891292+nargizmedetova@users.noreply.github.com>
2022-12-13 20:44:44 +0600

    added home/signIn/signUp/MyBooks

--- src/main/java/com/example/library_IAU/controller/MainController.java
+++ src/main/java/com/example/library_IAU/controller/MainController.java
@@ -24,29 +24,13 @@ public class MainController {
     Integer forLoad = 1;
     private ArrayList<Books> books = new ArrayList<Books>();
     private String url = "https://openlibrary.org/search?q=programming&mode=everything";
-
-
-    /*    public MainController(BooksService booksRepository) {
-        super();
-        this.booksRepository = (BooksRepository) booksRepository;
-    }*/
     private UsersService usersService;
-
     private BooksRepository booksRepository;
-
-/*    public MainController(UsersService usersService, BooksRepository booksRepository, BooksService booksService) {
-        this.usersService = usersService;
-        this.booksRepository = booksRepository;
-        this.booksService = booksService;
-    }*/
     public  MainController(BooksService booksService, UsersService usersService, BooksRepository booksRepository){
         this.booksService = booksService;
         this.usersService = usersService;
         this.booksRepository = booksRepository;
     }
-
-
-
     @GetMapping("/signIn")
     public String signIn(Model model){
 
@@ -65,31 +49,20 @@ public class MainController {
         System.out.println("usersModel is:" + usersModel);
         emailOfUser = usersModel.getEmail();
         UsersModel registeredUsers = usersService.registerUser(usersModel.getEmail(), usersModel.getPassword());
-        return registeredUsers == null?"signUp":"redirect:/";
+        return registeredUsers == null?"signUp":"/signIn";
     }
-
-
 String emailOfUser;
     @PostMapping("signIn")
     public String postSignIn(@ModelAttribute UsersModel usersModel){
-       // Books books = new Books();
-       // books.setId(1L);
-        //books.setName("new book");
-        System.out.println(usersModel +"the id is ------------------------------");
-
-        System.out.println("usersModel is:" + usersModel);
         UsersModel authenticate = usersService.authenticate(usersModel.getEmail(), usersModel.getPassword());
-        return authenticate == null?"signIn":"main";
+        return authenticate == null?"signIn":"redirect:/";
     }
-
-
     @RequestMapping(value="/do-stuff")
     public String doStuffMethod(Model model) throws Exception {
         books.clear();
         forLoad++;
         url = "https://openlibrary.org/search?mode=everything&q=programming&page="+forLoad.toString();
         return main(model);
-
     }
     @RequestMapping(value="/do-stuff2")
     public String doStuffMethod2(Model model) throws Exception {
@@ -102,42 +75,25 @@ String emailOfUser;
             url = "https://openlibrary.org/search?q=programming&mode=everything";
 
         books.clear();
-
         return main(model);
-
     }
     @GetMapping("/")
     public String main(Model model) throws Exception {
-        //books.clear();
         books.clear();
-
-
-
         RunPage();
-
-
-        //System.out.println(text.text());
         model.addAttribute("books",books);
-
         return "main";
     }
-
-
     @PostMapping("/")
     public String saveEmp(@ModelAttribute("employee") Books employee) {
         System.out.println(employee);
-booksService.saveBook(employee);
-
+        booksService.saveBook(employee);
         return "redirect:/";
     }
-
-
-
     @GetMapping("/forgotPass")
     public String forgotPass(Model model){
         return "forgotPass";
     }
-
     @GetMapping("/add/{id}")
     public String editEmpForm(@PathVariable Long id, Model model) {
         //model.addAttribute("employee", booksService.getBooksById(id));
@@ -145,7 +101,6 @@ booksService.saveBook(employee);
         addedBooks.add(addingBook);
         /*https://openlibrary.org/search?q=c%2B%2B&mode=everything
         * https://openlibrary.org/search?q=programming&mode=everything */
-
         return "redirect:/";
     }
     @GetMapping("/myBooks")
@@ -168,15 +123,12 @@ booksService.saveBook(employee);
             if(str.length() > 254){
                 str = str.substring(0, 253);
             }
-
             System.out.println(str);
             books1.setName(str);
             books1.setUserId(usersService.getUsersModelByEmail(emailOfUser).get().getId());
             books.add(books1);
             booksRepository.save(books1);
-            // books.add(element.text());
         }
-
     }
     public ArrayList<Books> getAddedBooks() {
         return addedBooks;
