2e67fc84178c6bf9121909ee0fdf1f5bb95e8441
raiym2048 <104891292+nargizmedetova@users.noreply.github.com>
2022-12-14 21:16:55 +0600

    added searching

--- src/main/java/com/example/library_IAU/controller/MainController.java
+++ src/main/java/com/example/library_IAU/controller/MainController.java
@@ -2,6 +2,7 @@ package com.example.library_IAU.controller;
 
 
 import com.example.library_IAU.model.Books;
+import com.example.library_IAU.model.Search;
 import com.example.library_IAU.model.UsersModel;
 import com.example.library_IAU.repository.BooksRepository;
 import com.example.library_IAU.service.BooksService;
@@ -23,7 +24,9 @@ public class MainController {
     private BooksService booksService;
     Integer forLoad = 1;
     private ArrayList<Books> books = new ArrayList<Books>();
-    private String url = "https://openlibrary.org/search?q=programming&mode=everything";
+    String searchingStr = "programming";
+    private String url = "https://openlibrary.org/search?q="+searchingStr+"&mode=everything";
+
     private UsersService usersService;
     private BooksRepository booksRepository;
     public  MainController(BooksService booksService, UsersService usersService, BooksRepository booksRepository){
@@ -31,11 +34,30 @@ public class MainController {
         this.usersService = usersService;
         this.booksRepository = booksRepository;
     }
+    @GetMapping("/search")
+    public String search(Model model){
+        String search = "";
+        model.addAttribute("search", search);
+        return "search";
+    }
+    @PostMapping("/search")
+    public String postSearch(@ModelAttribute Search search){
+        searchingStr = search.getSearch();
+
+        System.out.println(search.getSearch()+"uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu");
+        url = "https://openlibrary.org/search?q="+search.getSearch()+"&mode=everything";
+        return "redirect:/";
+    }
     @GetMapping("/signIn")
     public String signIn(Model model){
 
         model.addAttribute("signInRequest", new UsersModel());
         return "signIn";
+    }
+    @PostMapping("signIn")
+    public String postSignIn(@ModelAttribute UsersModel usersModel){
+        UsersModel authenticate = usersService.authenticate(usersModel.getEmail(), usersModel.getPassword());
+        return authenticate == null?"signIn":"redirect:/";
     }
     @GetMapping("/signUp")
     public String signUp(Model model){
@@ -52,44 +74,51 @@ public class MainController {
         return registeredUsers == null?"signUp":"/signIn";
     }
 String emailOfUser;
-    @PostMapping("signIn")
-    public String postSignIn(@ModelAttribute UsersModel usersModel){
-        UsersModel authenticate = usersService.authenticate(usersModel.getEmail(), usersModel.getPassword());
-        return authenticate == null?"signIn":"redirect:/";
-    }
+
     @RequestMapping(value="/do-stuff")
     public String doStuffMethod(Model model) throws Exception {
         books.clear();
         forLoad++;
-        url = "https://openlibrary.org/search?mode=everything&q=programming&page="+forLoad.toString();
+        url = "https://openlibrary.org/search?mode=everything&q="+searchingStr+"&page="+forLoad.toString();
+             /*https://openlibrary.org/search?q=programming&mode=everything*/
         return main(model);
     }
     @RequestMapping(value="/do-stuff2")
     public String doStuffMethod2(Model model) throws Exception {
         if(forLoad > 2){
             forLoad--;
-            url = "https://openlibrary.org/search?mode=everything&q=programming&page="+forLoad.toString();
+            url = "https://openlibrary.org/search?mode=everything&q="+searchingStr+"&page="+forLoad.toString();
         }
 
         else
-            url = "https://openlibrary.org/search?q=programming&mode=everything";
+            url = "https://openlibrary.org/search?q="+searchingStr+"&mode=everything";
 
         books.clear();
         return main(model);
     }
+    @GetMapping("/allBooks")
+    public String allBooks(Model model) throws IOException {
+        searchingStr = "programming";
+        url = "https://openlibrary.org/search?q="+searchingStr+"&mode=everything";
+        //RunPage();
+        return "redirect:/";
+    }
     @GetMapping("/")
     public String main(Model model) throws Exception {
         books.clear();
-        RunPage();
+        RunPage(getUrl());
         model.addAttribute("books",books);
         return "main";
     }
-    @PostMapping("/")
+    /*@PostMapping("/")
     public String saveEmp(@ModelAttribute("employee") Books employee) {
         System.out.println(employee);
         booksService.saveBook(employee);
         return "redirect:/";
-    }
+    }*/
+
+
+
     @GetMapping("/forgotPass")
     public String forgotPass(Model model){
         return "forgotPass";
@@ -98,7 +127,18 @@ String emailOfUser;
     public String editEmpForm(@PathVariable Long id, Model model) {
         //model.addAttribute("employee", booksService.getBooksById(id));
         Books addingBook =  booksRepository.getById(id);
+        System.out.println(addingBook + "akhdadhkahadhadjadhj");
+        for(Books b:addedBooks){
+            if(b.getName().equals(addingBook.getName())){
+                return "redirect:/";
+            }
+        }
+//        if(addedBooks.contains(addingBook)){
+//
+//
+//        }
         addedBooks.add(addingBook);
+
         /*https://openlibrary.org/search?q=c%2B%2B&mode=everything
         * https://openlibrary.org/search?q=programming&mode=everything */
         return "redirect:/";
@@ -109,7 +149,7 @@ String emailOfUser;
         return "myBooks";
     }
 
-    public void RunPage() throws IOException {
+    public void RunPage(String url) throws IOException {
         org.jsoup.nodes.Document page = Jsoup.parse(new URL(url), 15000);
         //  Element main = page.select("div[id=phpSonuclariGoster]").first();
         Elements rows = page.select("div[class=details]");
@@ -125,7 +165,11 @@ String emailOfUser;
             }
             System.out.println(str);
             books1.setName(str);
+/*            if(usersService.getUsersModelByEmail(emailOfUser).get().getId().equals(null)){
+
+            }*/
             books1.setUserId(usersService.getUsersModelByEmail(emailOfUser).get().getId());
+
             books.add(books1);
             booksRepository.save(books1);
         }
---
+++ src/main/java/com/example/library_IAU/model/Search.java
@@ -0,0 +1,13 @@
+package com.example.library_IAU.model;
+
+public class Search {
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+
+    String search;
+}
